043f8f6f3079183504180db19097d94a70da739b
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-28 15:40:04 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from telegram.ext import (
     Application,
-    CommandHandler,
     MessageHandler,
     filters,
 )
 
 from config import BOT_TOKEN
+
 from handlers.start import register_handler
 from handlers.komisi import komisi
 from handlers.withdraw import withdraw_handler
@@ -15,11 +15,15 @@ def main():
 
     app = Application.builder().token(BOT_TOKEN).build()
 
-    # Command
     app.add_handler(register_handler)
 
-    # Button
-    app.add_handler(MessageHandler(filters.Regex("^💰 Cek Komisi$"), komisi))
+    app.add_handler(
+        MessageHandler(
+            filters.Regex("^💰 Cek Komisi$"),
+            komisi
+        )
+    )
+
     app.add_handler(withdraw_handler)
 
     print("Bot Running...")
