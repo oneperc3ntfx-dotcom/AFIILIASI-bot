6982a706daa68034945ab9d68d0c9d9d1d74d8bb
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-28 15:25:25 +0700

    Create appscript.py

---
+++ services/appscript.py
@@ -0,0 +1,37 @@
+import requests
+from config import APPS_SCRIPT_URL
+
+
+def register(wallet, gmail, telegram):
+    payload = {
+        "action": "register",
+        "wallet": wallet,
+        "gmail": gmail,
+        "telegram": telegram
+    }
+
+    r = requests.post(APPS_SCRIPT_URL, data=payload)
+    return r.json()
+
+
+def get_komisi(telegram):
+    payload = {
+        "action": "komisi",
+        "telegram": telegram
+    }
+
+    r = requests.post(APPS_SCRIPT_URL, data=payload)
+    return r.json()
+
+
+def withdraw(telegram, nominal, bank="", rekening=""):
+    payload = {
+        "action": "withdraw",
+        "telegram": telegram,
+        "nominal": nominal,
+        "bank": bank,
+        "rekening": rekening
+    }
+
+    r = requests.post(APPS_SCRIPT_URL, data=payload)
+    return r.json()
