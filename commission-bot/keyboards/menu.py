941bf4a3c94d4441bdebf48546bbee80604ef006
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-28 15:29:40 +0700

    Update menu.py

--- commission-bot/keyboards/menu.py
+++ commission-bot/keyboards/menu.py
@@ -1,1 +1,14 @@
+from telegram import ReplyKeyboardMarkup
 
+
+def main_menu():
+    keyboard = [
+        ["💰 Cek Komisi"],
+        ["🏦 Withdraw"]
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True,
+        one_time_keyboard=False
+    )
