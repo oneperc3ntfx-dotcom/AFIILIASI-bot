f2b65257f7047f6c90932d676582dc8e4e1f2a9a
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-29 14:34:52 +0700

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -2,13 +2,18 @@ from telegram import ReplyKeyboardMarkup
 
 
 def main_menu():
+
     keyboard = [
-        ["💰 Cek Komisi"],
-        ["🏦 Withdraw"]
+
+        ["💰 Komisi", "🏦 Withdraw"],
+
+        ["📞 Support", "ℹ️ Bantuan"]
+
     ]
 
     return ReplyKeyboardMarkup(
         keyboard,
         resize_keyboard=True,
-        one_time_keyboard=False
+        one_time_keyboard=False,
+        selective=False
     )
