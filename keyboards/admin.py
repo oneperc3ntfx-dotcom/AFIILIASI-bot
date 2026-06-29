84e0c3f35ea75e253d9a68c68c18429ea183086e
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-29 14:29:40 +0700

    Create admin.py

---
+++ keyboards/admin.py
@@ -0,0 +1,21 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def withdraw_admin_keyboard(wd_id):
+
+    keyboard = [
+
+        [
+            InlineKeyboardButton(
+                "✅ Done",
+                callback_data=f"wd_done:{wd_id}"
+            ),
+            InlineKeyboardButton(
+                "❌ Reject",
+                callback_data=f"wd_reject:{wd_id}"
+            )
+        ]
+
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
